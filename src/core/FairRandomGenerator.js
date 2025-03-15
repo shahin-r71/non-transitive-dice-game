2658838459acd167923bd1a7a1c2224d67ab0128
shahin-r71 <shahinlv01@gmail.com>
2025-03-15 19:06:56 +0600

    feat: create FairRandomGenerator class

---
+++ src/core/FairRandomGenerator.js
@@ -0,0 +1,30 @@
+import crypto from 'node:crypto';
+export class FairRandomGenerator {
+  constructor(){
+    this.key = null;
+    this.computerNumber = null;
+    this.hmac = null;
+  }
+  generateKey(){
+    this.key = crypto.randomBytes(32);
+    // return this.key;
+  }
+  generateComputerNumber(maxValue){
+    this.computerNumber = crypto.randomInt(0,maxValue+1);
+    // return this.computerNumber;
+  }
+  generateHmac(){
+    if (!this.key || this.computerNumber === null) throw new Error('Key and computer number must be set before calculating HMAC');
+    this.hmac = crypto.createHmac('sha3-256', this.key).update(this.computerNumber.toString()).digest('hex');
+    return this.hmac;
+  }
+  getComputerNumber(){
+    return this.computerNumber;
+  }
+  getHmac(){
+    return this.hmac;
+  }
+  getKey(){
+    return this.key;
+  }
+}
