061087d83f4b290dc4271eb59e61b3f774286b99
shahin-r71 <shahinlv01@gmail.com>
2025-03-15 16:20:16 +0600

    feat: add dice class

---
+++ src/core/Dice.js
@@ -0,0 +1,19 @@
+export class Dice {
+  constructor(faces) {
+    if(!Array.isArray(faces) || faces.length !==6) {
+      throw new Error('Invalid dice configuration, must be an array of arrays each containing 6 faces');
+    }
+    this.faces = faces;
+    // console.log(this.faces);
+  }
+
+  static toNumber(diceString) {
+    const faces = diceString.split(',').map(face =>{
+      const value = Number(face.trim());
+      if(isNaN(value) || !Number.isInteger(value))throw new Error(`Invalid face value: "${value}". Face values must be integers. For example, use "2,2,4,4,9,9".`)
+      return value;        
+    })
+    return new Dice(faces);
+  }
+}
+
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import { DiceParser } from './core/DiceParser.js';
-
+import { Dice } from './core/Dice.js';
 const diceParser = new DiceParser();
 const diceconfigs = diceParser.parseDiceConfigs();
 
-console.log(diceconfigs);
+const dice= diceconfigs.map(d=>Dice.toNumber(d));
+
+console.log(dice)
+// console.log(diceconfigs[1]);
 
